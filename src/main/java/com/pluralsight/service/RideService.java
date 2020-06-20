0c31c56b441041c8f0c231ddd8ff77148e9d9902
Vasileios Ntogram <33256755+s1720094@users.noreply.github.com>
2020-06-20 16:11:21 +0300

    Module6 changes

--- src/main/java/com/pluralsight/service/RideService.java
+++ src/main/java/com/pluralsight/service/RideService.java
@@ -9,5 +9,11 @@ public interface RideService {
 	Ride createRide(Ride ride);
 	
 	List<Ride> getRides();
+	
+	Ride getRide(Integer id);
+
+	Ride updateRide(Ride ride);
+
+	void batch();
 
 }
--- src/test/java/com/pluralsight/controller/RestControllerTest.java
+++ src/test/java/com/pluralsight/controller/RestControllerTest.java
@@ -38,4 +38,35 @@ public class RestControllerTest {
 			System.out.println("Ride name: " + ride.getName());
 		}
 	}
+	
+	@Test(timeout=3000)
+	public void testGetRide() {
+		RestTemplate restTemplate = new RestTemplate();
+		
+		Ride ride = restTemplate.getForObject("http://localhost:8080/ride_tracker/ride/1", Ride.class);
+		
+		System.out.println("Ride name: " + ride.getName());
+	}
+	
+	@Test(timeout=3000)
+	public void testUpdateRide() {
+		RestTemplate restTemplate = new RestTemplate();
+		
+		Ride ride = restTemplate.getForObject("http://localhost:8080/ride_tracker/ride/1", Ride.class);
+		
+		ride.setDuration(ride.getDuration() + 1);
+		
+		restTemplate.put("http://localhost:8080/ride_tracker/ride", ride);
+		
+		System.out.println("Ride name: " + ride.getName());
+	}
+	
+	@Test(timeout=3000)
+	public void testBatchUpdate() {
+		RestTemplate restTemplate = new RestTemplate();
+		
+		restTemplate.getForObject("http://localhost:8080/ride_tracker/batch", Object.class);
+	
+	}
+	
 }
